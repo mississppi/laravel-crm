ee29b6b2841d64b748c4a19c54a7d961f81768f5
mississppi <msspstudio@gmail.com>
2025-01-07 09:42:32 +0900

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,6 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',  // Vueファイルを対象にする場合
-        './resources/js/**/*.jsx',  // JSXファイルを対象にする場合
-        './resources/js/**/*.ts',   // TypeScriptを対象にする場合
-        './resources/js/**/*.tsx',  // TypeScriptとJSXの組み合わせを対象にする場合
-        './resources/css/**/*.css', // CSSファイルも対象にする場合
     ],
 
     theme: {
--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',  // コンテナ外からもアクセスできるようにする
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0', // コンテナ内でのアクセスを許可
-        port: 5173, // Viteのデフォルトポート
-        strictPort: true, // このポートで確実に起動
-        hmr: {
-            host: 'localhost', // ホットモジュールリロード用のホスト名
-        },
-        watch: {
-            usePolling: true, // ファイル変更をポーリングで監視
-        },
-    },
 });
